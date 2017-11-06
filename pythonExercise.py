f543eb112aa0a49e4385d9324e25b2aa0a161c28
HangYang0204 <yangdehang0204@gmail.com>
2017-11-06 14:29:47 -0500

    Add files via upload

---
+++ pythonExercise.py
@@ -0,0 +1,38 @@
+#python exerciese
+#https://www.ynonperek.com/2017/09/21/python-exercises/
+import sys
+#1. write a program to take a task that ask user for a number and prints the sum
+# ot its digits. (int)
+#2. wreite a program that takes a file name as command line argument, count how
+#many times each word appears in the file and prints the word that appears the
+#most
+
+
+
+class solution(object):
+    """Exercise object"""
+    def sumdigits(self):
+        """Exercise 1"""
+        intP = input('Enter your number: ')
+        l = []
+        for char in intP:
+            l.append(int(char))
+        self = sum(l)
+        print(self)
+    def countwords():
+        """Exercise 2"""
+
+
+
+
+
+
+s = solution()
+s.sumdigits()
+
+
+
+
+
+
+
